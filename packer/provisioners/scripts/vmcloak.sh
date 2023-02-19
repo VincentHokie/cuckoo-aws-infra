c101a58934d16970d2a029e02a107f71b49970f0
Vincent Asante <vincenthokie@gmail.com>
2023-02-19 10:40:43 +0300

    let vmcloak script just set up base vms that could be used out of the box

--- packer/provisioners/scripts/vmcloak.sh
+++ packer/provisioners/scripts/vmcloak.sh
@@ -1,18 +1,5 @@
 # VM setup
 
-wget --output-document win7x64.iso --continue https://cuckoo.sh/win7ultimate.iso
-
-wget --output-document win7x86.iso --continue https://ss2.softlay.com/files/en_windows_7_ultimate_x86_dvd.iso
-
-wget --output-document win81x64.iso --continue https://ss2.softlay.com/files/en_windows_8_1_pro_vl_x64_dvd_2971948.iso
-
-wget --output-document win81x86.iso --continue https://ss2.softlay.com/files/en_windows_8_1_pro_vl_x86_dvd_2972633.iso
-
-wget --output-document win10x86.iso --continue https://ss2.softlay.com/files/en_Windows_10_1607_build_14393_x32_dvd.iso
-
-wget --output-document win10x64.iso --continue https://ss2.softlay.com/files/en_Windows_10_1607_build_14393_x64_dvd.iso
-
-
 sudo apt-get -y install genisoimage
 # sudo apt-get update
 # sudo apt-get upgrade -y
