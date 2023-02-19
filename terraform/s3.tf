cf144b3df0af1ca8ff0872d55ccfff9331fd7b74
Vincent Asante <vincenthokie@gmail.com>
2023-02-19 18:07:18 +0300

    enforce encrypted communication with the s3 buckets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -48,3 +48,51 @@ resource "aws_s3_bucket_public_access_block" "block_public_cuckoo_vms" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "enforce_encryped_communication_analyses_reports" {
+  bucket = aws_s3_bucket.analyses_reports.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AnalysesMustBeEncryptedInTransit"
+        Action = "s3:*"
+        Principal = "*"
+        Effect   = "Deny"
+        Resource = [
+          aws_s3_bucket.analyses_reports.arn,
+          "${aws_s3_bucket.analyses_reports.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_policy" "enforce_encryped_communication_cuckoo_vms" {
+  bucket = aws_s3_bucket.cuckoo_vms.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "VMTrafficMustBeEncryptedInTransit"
+        Action = "s3:*"
+        Principal = "*"
+        Effect   = "Deny"
+        Resource = [
+          aws_s3_bucket.cuckoo_vms.arn,
+          "${aws_s3_bucket.cuckoo_vms.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
