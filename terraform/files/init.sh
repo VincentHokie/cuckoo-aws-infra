0184b826355ee296cb85e5fdd5d56deaa3421bd0
Vincent Asante <vincenthokie@gmail.com>
2023-03-25 18:42:26 +0300

    create vm init changes

--- terraform/files/init.sh
+++ terraform/files/init.sh
@@ -31,6 +31,35 @@ cd cuckoo
 git checkout final-project-alterations
 python stuff/monitor.py
 pip install .
+
+cd /home/ubuntu/vmcloak/
+git checkout project-fixes
+git fetch
+git pull
+pip install .
+
+cat >/home/ubuntu/vmcloak.sh <<EOL
+#!/bin/bash
+
+ram=\$1
+osarch=\$2
+osversion=\$3
+vmname=\$4
+cpu=\$5
+vdi=\$6
+
+. /home/ubuntu/venv/bin/activate
+
+HOME=/home/ubuntu vmcloak init --verbose --win\${osversion}x\${osarch} custom-\${vmname} --cpus \$cpu --ramsize \$ram --vdifile \$vdi
+HOME=/home/ubuntu vmcloak clone custom-\${vmname} custom-\${vmname}-cuckoo
+HOME=/home/ubuntu vmcloak install custom-\${vmname}-cuckoo adobepdf pillow dotnet java flash vcredist vcredist.version=2015u3 wallpaper ie11
+HOME=/home/ubuntu vmcloak snapshot --count 1 custom-\${vmname}-cuckoo custom-\${vmname}-cuckoo-win\${osversion}x\${osarch} 192.168.56.100
+cuckoo machine --add --resultserver "192.168.56.1:2042" custom-\${vmname}-cuckoo-win\${osversion}x\${osarch} 192.168.56.100
+EOL
+
+chmod +x /home/ubuntu/vmcloak.sh
+chown ubuntu:ubuntu /home/ubuntu/vmcloak.sh
+
 touch /home/ubuntu/venv/local/lib/python2.7/site-packages/cuckoo/private/.cwd
 
 supervisord -c /home/ubuntu/.cuckoo/supervisord.conf
